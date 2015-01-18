734b5873d4dd4b88e0d8dc8cef78a0e8ad4e8e16
Steny Liu <steny138@gmail.com>
2015-01-18 23:26:25 +0800

    some thing is loss is past again

---
+++ TicketRobot.Core/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketRobot.Core
+{
+    public class AppSettings
+    {
+        public static string AEUrl
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings.Get("AE");
+            }
+        }
+    }
+}
---
+++ TicketRobot.Core/Utility.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO; 
+using HtmlAgilityPack;
+using System.Threading;
+
+namespace TicketRobot.Core
+{
+    public class Utility
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        /// <summary>載入提供的網址的HTML並轉換成HTML Document-Get</summary>
+        /// <param name="url">網址</param>
+        /// <param name="encoding">編碼類型</param>
+        public static HtmlDocument downLoadHtmlDoc(string url, Encoding encoding)
+        {
+            WebClient client = new WebClient();
+            HtmlDocument doc = new HtmlDocument();
+            try
+            {
+                using (MemoryStream ms = new MemoryStream(client.DownloadData(url)))
+                {
+                    Thread.Sleep(100);
+                    //使用UTF8反而會變亂碼，要用Default
+                    doc.Load(ms, encoding);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Trace(string.Format("Download url 失敗 : {0}", url));
+            }
+            return doc;
+        }
+
+        /// <summary>載入提供的網址的HTML並轉換成HTML Document-Post</summary>
+        /// <param name="url">網址</param>
+        /// <param name="encoding">編碼類型</param>
+        public static HtmlDocument downLoadHtmlDoc(string url, string postBody, Encoding encoding)
+        {
+            WebClient client = new WebClient();
+            HtmlDocument doc = new HtmlDocument();
+            try
+            {
+                client.Encoding = encoding;
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+                byte[] sendData = encoding.GetBytes(postBody);
+                using (MemoryStream ms = new MemoryStream(client.UploadData(url, "POST", sendData)))
+                {
+                    Thread.Sleep(100);
+                    //使用UTF8反而會變亂碼，要用Default
+                    doc.Load(ms, encoding);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Trace(string.Format("Download url 失敗 : {0}", url));
+            }
+            return doc;
+        }
+
+        public static void writeLog(string content)
+        {
+            logger.Info(content);
+        }
+    }
+}
