23109564abd531e012ca468b8a932ee1a0a4df35
Steny <stenyliu@liontravel.com>
2015-01-19 17:11:12 +0800

    Implement the order step with webbrowser

--- TicketRobot/Form1.Designer.cs
+++ TicketRobot/Form1.Designer.cs
@@ -31,34 +31,46 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.SpiderButton = new System.Windows.Forms.Button();
+            this.OrderButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // webBrowser
             // 
-            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser.Location = new System.Drawing.Point(0, 0);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.webBrowser.Location = new System.Drawing.Point(1, 101);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 120);
             this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(235, 175);
+            this.webBrowser.Size = new System.Drawing.Size(782, 315);
             this.webBrowser.TabIndex = 0;
             this.webBrowser.Visible = false;
             this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
             // 
             // SpiderButton
             // 
-            this.SpiderButton.Location = new System.Drawing.Point(65, 79);
+            this.SpiderButton.Location = new System.Drawing.Point(330, 50);
             this.SpiderButton.Name = "SpiderButton";
-            this.SpiderButton.Size = new System.Drawing.Size(75, 23);
+            this.SpiderButton.Size = new System.Drawing.Size(120, 23);
             this.SpiderButton.TabIndex = 2;
-            this.SpiderButton.Text = "Execute";
+            this.SpiderButton.Text = "Execute - Flight Table";
             this.SpiderButton.UseVisualStyleBackColor = true;
             this.SpiderButton.Click += new System.EventHandler(this.SpiderButton_Click);
+            // 
+            // OrderButton
+            // 
+            this.OrderButton.Location = new System.Drawing.Point(330, 21);
+            this.OrderButton.Name = "OrderButton";
+            this.OrderButton.Size = new System.Drawing.Size(120, 23);
+            this.OrderButton.TabIndex = 3;
+            this.OrderButton.Text = "Execute-Order";
+            this.OrderButton.UseVisualStyleBackColor = true;
+            this.OrderButton.Click += new System.EventHandler(this.OrderButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(235, 175);
+            this.ClientSize = new System.Drawing.Size(782, 415);
+            this.Controls.Add(this.OrderButton);
             this.Controls.Add(this.SpiderButton);
             this.Controls.Add(this.webBrowser);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -73,6 +85,7 @@
 
         private System.Windows.Forms.WebBrowser webBrowser;
         private System.Windows.Forms.Button SpiderButton;
+        private System.Windows.Forms.Button OrderButton;
     }
 }
 
--- TicketRobot/Form1.cs
+++ TicketRobot/Form1.cs
@@ -28,17 +28,166 @@ namespace TicketRobot
 
         private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            
+            WebBrowser browser = (WebBrowser)sender;
+            string url = browser.Url.LocalPath;
+            switch(url)
+            {
+                case "/uniairec/b2c/cfresav01.aspx":
+                    //首頁
+                    insertText("UniCalendar1_txtYear", "2015"); //去程日期 - 年
+                    insertText("UniCalendar1_txtMonth", "01"); //去程日期 - 月
+                    insertText("UniCalendar1_txtDay", "30"); //去程日期 - 日
+                    insertText("UniCalendar2_txtYear", "2015"); //回程日期 - 年
+                    insertText("UniCalendar2_txtMonth", "01"); //回程日期 - 月
+                    insertText("UniCalendar2_txtDay", "30"); //回程日期 - 日
+                    insertText("Radio4", "1"); //回程日期 - 日
+                    
+                    setSelectOption("item1", "TSA"); //出發地
+                    
+                    setSelectOption("item2", "TTT"); //目的地
+                    setSelectOption("ddm_adultNum", "1"); //成人
+                    setSelectOption("ddm_childNum", "0"); //兒童
+                    setSelectOption("ddm_oldNum", "0"); //敬老
+                    setSelectOption("ddm_offIndNum", "0"); //軍人
+                    setSelectOption("ddm_SBNum", "0"); //愛心
+                    setSelectOption("ddm_offIndNum", "0"); //居民
+                    setSelectOption("ddm_IENum", "0"); //愛心陪同
+                    buttonClick("Button1");
+                    break;
+                case "/uniairec/b2c/cfresav02.aspx":
+                    //選擇航班
+                    buttonClick("Button1");
+                    break;
+                case "/uniairec/b2c/cfresav03a.aspx":
+                    //閱讀注意事項
+                    checkboxClick("checkbox");
+                    buttonClick("Button1");
+                    break;
+                case "/uniairec/b2c/cfresav03.aspx":
+                    //旅客資料
+                    insertText("txt_famName_1", "劉"); //姓
+                    insertText("txt_gvnName_1", "宥辰"); //名
+                    insertText("ddm_year_1", "1989"); //生日 - 年
+                    insertText("ddm_month_1", "05"); //生日 - 月
+                    insertText("ddm_day_1", "04"); //生日 - 日
+                    insertText("txt_idNum_1", "N124885927"); //身分證
+                    insertText("txt_pptNum_1", ""); //外籍旅客護照號碼
+                    setSelectOption("ddm_title_1", "01");
+
+                    //---------聯絡人資料
+                    setSelectOption("samewith", "1"); //同第幾位旅客資料
+                    insertText("areacode", "02"); //區碼
+                    insertText("phonenum", "87939000"); //市內電話
+                    insertText("ext", "8338"); //分機
+                    insertText("cellnum", "0911757685"); //手機號碼
+                    setSelectOption("cellnum_na", "886/"); //國碼
+                    insertText("email", "steny138@gmail.com"); //電子郵件
+                    
+
+                    //buttonClick("Button1");
+                    break;
+
+            }
+            url = "te";
         }
 
+        #region 輸入欄位
+        public void insertText(string element, string value)
+        {
+            try
+            {
+                webBrowser.Document.Body.All[element].InnerText = value;
+                //輸入至欄位
+            }
+            catch
+            {
+                //status(result);
+                //發生錯誤，輸出至狀態
+            }
+        }
+
+        public void setSelectOption(string element, string value)
+        {
+            try
+            {
+                var dropdown = webBrowser.Document.GetElementById(element);
+                if (dropdown == null)
+                {
+                    dropdown = webBrowser.Document.GetElementsByTagName("select")[element];
+                }
+
+                if (dropdown != null)
+                {
+                    var dropdownItems = dropdown.GetElementsByTagName("option");
+
+                    foreach (HtmlElement option in dropdownItems)
+                    {
+                        var originValue = option.GetAttribute("value").ToString();
+                        if (originValue.Equals(value))
+                        {
+                            option.SetAttribute("selected", "selected");
+                            dropdown.RaiseEvent("onChange");
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
+        }
+
+        public void buttonClick(string id)
+        {
+            webBrowser.Document.GetElementById(id).InvokeMember("click");
+        }
+
+        public void checkboxClick(string id)
+        {
+            var checkbox = webBrowser.Document.GetElementsByTagName("input").GetElementsByName(id);
+            if (checkbox != null && checkbox.Count > 0)
+            {
+                checkbox[0].InvokeMember("click");
+            }
+        }
+        #endregion
+
+        #region 按鈕事件
+       
         private void SpiderButton_Click(object sender, EventArgs e)
         {
             SpiderButton.Text = "Executing...";
+            OrderButton.Enabled = false;
             SpiderButton.Enabled = false;
             ThreadPool.QueueUserWorkItem(new WaitCallback(startJob), "0");
-            
         }
-
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            OrderButton.Text = "Executing...";
+            OrderButton.Enabled = false;
+            SpiderButton.Enabled = false;
+            webBrowser.Visible = true;
+            //自動操作
+            webBrowser.Navigate("https://www.uniair.com.tw/uniairec/b2c/cfresav01.aspx");
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(startOrder), "0");
+        }
+        private void startOrder(object state)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                Utility.writeErrorLog(string.Format("程式錯誤: {0}", ex.ToString()));
+            }
+            finally
+            {
+                this.Invoke(
+                 new UpdateLableHandler(finishExecuteOrder),
+                 "1");
+            }
+        }
 
         private void startJob(object state)
         {
@@ -93,16 +242,25 @@ namespace TicketRobot
             finally
             {
                 this.Invoke(
-                 new UpdateLableHandler(finishExecute),
+                 new UpdateLableHandler(finishExecuteSpider),
                  "1");
             }
         }
         delegate void UpdateLableHandler(string text);
-        private void finishExecute(string text)
+        private void finishExecuteSpider(string text)
         {
+            OrderButton.Enabled = true;
             SpiderButton.Enabled = true;
-            SpiderButton.Text = "Execute";
+            webBrowser.Visible = false;
+            SpiderButton.Text = "Execute - FlightTable";
         }
-
+        private void finishExecuteOrder(string text)
+        {
+            OrderButton.Enabled = true;
+            SpiderButton.Enabled = true;
+            webBrowser.Visible = false;
+            SpiderButton.Text = "Execute - Order";
+        }
+        #endregion
     }
 }
